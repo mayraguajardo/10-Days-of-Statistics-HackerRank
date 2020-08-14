42bd39fd0185012dfd2de3a49c9a90b4cf6704d7
MayraGuajardo <A01634475@itesm.mx>
2020-08-13 22:26:49 -0500

    Day 0 Mean, Median & Mode

---
+++ mean_median_mode.py
@@ -0,0 +1,44 @@
+x = int(input()) # size of array
+numbers = list(map(float, input().split())) # converting list to float
+
+
+def getMean(list):
+    sum = 0
+    for number in list:
+        sum += number
+    mean = float(sum)/len(list)
+    return mean
+
+def getMedian(list):
+    median = 0.0
+    size = len(list)
+    copy = list
+    copy.sort()
+    if (size % 2 == 0):
+        median = float(copy[size//2-1] + copy[size//2])/2
+    else:
+        median = copy[(size-1)/2]
+    return median
+
+def getMode(list):
+    mode = 0
+    size = len(list)
+    count,max = 0,0
+    copy = list
+    copy.sort()
+    current = 0
+    for i in copy:
+        if (i == current):
+            count += 1
+        else:
+            count = 1
+            current = i
+        if (count > max):
+            max = count
+            mode = i
+    return mode
+
+
+print(getMean(numbers))
+print(getMedian(numbers))
+print(getMode(numbers))
